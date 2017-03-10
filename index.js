0ce02d0086a4ac2c7b5a7882b91780580cb3c5f9
Hawaii98 <hawaii@pdot.eu>
2017-03-10 11:19:46 +0100

    fix dir to public content

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + 'public/index.html');
 });
 
 io.on('connection', function(socket){
