1e09828b8dd777e95e8b537a367d82c355b1d77b
Hawaii98 <hawaii@pdot.eu>
2017-03-21 21:32:42 +0100

    added create game form

--- public/lobby.js
+++ public/lobby.js
@@ -2,8 +2,20 @@ var lobby = {
 	vue: new Vue({
 		el: '#lobby',
 		data: {
-			gameList: []
-		}
+			gameList: [],
+            newGame_m: null,
+            newGame_n: null,
+            newGame_k: null,
+            newGame_id: null
+		},
+        methods: {
+            createGame: function () {
+            	lobby.createGame(this.newGame_m, this.newGame_n, this.newGame_k, this.newGame_id);
+            },
+            joinGame: function () {
+            	lobby.joinGame();
+            }
+        }
 	}),
 	obj: document.getElementById('lobby'),
 	showLobby: function (){
@@ -12,5 +24,11 @@ var lobby = {
 	},
 	hideLobby: function (){
 		lobby.obj.style.display = "none";
-	}
+	},
+	createGame: function (m, n, k, id) {
+		socket.emit("createRoom", {m: m, n: n, k: k, id: id});
+    },
+	joinGame: function (id) {
+		socket.emit("joinRoom", {gameId: id});
+    }
 }
